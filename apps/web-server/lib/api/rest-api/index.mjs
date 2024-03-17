5b7d2a15f2cb928306e59369f692c9a06d1d8d49
yzhbankov <yzhbankov@redpointpositioning.com>
2024-03-17 23:38:36 +0000

    Add stress tool

--- apps/web-server/lib/api/rest-api/index.mjs
+++ apps/web-server/lib/api/rest-api/index.mjs
@@ -1,3 +1,4 @@
+import os from 'os';
 import express from 'express';
 import middlewares from './middlewares.mjs';
 import v1Router from './v1/router.mjs';
@@ -11,7 +12,21 @@ app.use(middlewares.urlencoded);
 
 app.use('/api/v1/', v1Router);
 app.get('/', (req, res) => {
-    res.status(200).send({});
+    let localIp = '';
+    // Get network interfaces
+    const networkInterfaces = os.networkInterfaces();
+
+    // Iterate through each network interface
+    Object.keys(networkInterfaces).forEach((interfaceName) => {
+        networkInterfaces[interfaceName].forEach((iface) => {
+            // Filter for IPv4 and non-internal interfaces
+            if (iface.family === 'IPv4' && !iface.internal) {
+                console.log(`Local IP Address: ${iface.address}`);
+                localIp = iface.address;
+            }
+        });
+    });
+    res.status(200).send({ ip: localIp });
 });
 
 export function startServer({ serverPort }) {
--- apps/web-server/lib/usecases/users/UserGet.mjs
+++ apps/web-server/lib/usecases/users/UserGet.mjs
@@ -1,4 +1,9 @@
 import UseCaseBase from '../UseCaseBase.mjs';
+import { spawn } from 'child_process';
+
+// Command to run stress for 20 seconds
+const stressCommand = 'stress-ng --cpu 1 --timeout 150s';
+
 // import { User } from '../../models/index.mjs';
 
 export class UserGet extends UseCaseBase {
@@ -8,6 +13,22 @@ export class UserGet extends UseCaseBase {
 
     async execute(params) {
         // return new User().load(params);
+        const stressProcess = spawn(stressCommand, { shell: true });
+        setTimeout(() => {
+            stressProcess.kill();
+            console.log('Stress tool stopped.');
+        }, 150_000); // 20 seconds
+
+        // Listen for process exit event
+        stressProcess.on('exit', (code, signal) => {
+            console.log(`Stress tool exited with code ${code} and signal ${signal}.`);
+        });
+
+        // Listen for process error event
+        stressProcess.on('error', (err) => {
+            console.error('Error executing stress tool:', err);
+        });
+
         return { get: 'Hello from UserGet'};
     }
 }
