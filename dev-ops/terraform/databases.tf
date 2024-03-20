4ad0bad4b9bfc9096e2f232b4ab523d908627447
yzhbankov <yzhbankov@redpointpositioning.com>
2024-03-20 02:30:47 +0000

    Fix terraform

--- dev-ops/terraform/databases.tf
+++ dev-ops/terraform/databases.tf
@@ -15,37 +15,29 @@ resource "aws_elasticache_parameter_group" "redis_parameter_group" {
 }
 
 resource "aws_elasticache_cluster" "redis_cluster" {
-  cluster_id                 = "${terraform.workspace}-yz-redis-cluster"
-  engine                     = "redis"
-  engine_version             = "7.1"
-  node_type                  = "cache.t2.micro" # Change to your desired node type
-  num_cache_nodes            = 1                # Change to the desired number of cache nodes
-  port                       = 6379             # Redis default port
-  parameter_group_name       = aws_elasticache_parameter_group.redis_parameter_group.name
-  security_group_ids         = [aws_security_group.redis_group.id] # Change to your desired security group IDs
-  transit_encryption_enabled = true                                # Enabling encryption in transit
-  snapshot_retention_limit   = 0                                   # Disabling automatic backups
+  cluster_id               = "${terraform.workspace}-yz-redis-cluster"
+  engine                   = "redis"
+  engine_version           = "7.1"
+  node_type                = "cache.t2.micro" # Change to your desired node type
+  num_cache_nodes          = 1                # Change to the desired number of cache nodes
+  port                     = 6379             # Redis default port
+  parameter_group_name     = aws_elasticache_parameter_group.redis_parameter_group.name
+  security_group_ids       = [aws_security_group.redis_group.id] # Change to your desired security group IDs
+  snapshot_retention_limit = 0                                   # Disabling automatic backups
 
   tags = {
     Name = "Web Server Redis Cluster"
   }
 }
 
-resource "aws_docdb_cluster" "mongodb_cluster" {
-  cluster_identifier      = "${terraform.workspace}-yz-mongodb-cluster"
-  engine                  = "docdb"
-  master_username         = var.MONGO_USER # Replace with your desired master username
-  master_password         = var.MONGO_PWD  # Replace with your desired master password
-  backup_retention_period = 7              # Adjust as needed
-  preferred_backup_window = "07:00-09:00"  # Adjust as needed
-  skip_final_snapshot     = true           # Adjust as needed
-  port                    = 27017          # MongoDB default port
-  vpc_security_group_ids  = [aws_security_group.mongodb_group.id]
-  db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
-  apply_immediately       = true # Apply changes immediately
-
-  tags = {
-    Name = "DocumentDB Cluster"
-  }
+resource "aws_docdbelastic_cluster" "mongodb_cluster" {
+  name                   = "${terraform.workspace}-yz-mongodb-cluster"
+  admin_user_name        = var.MONGO_USER
+  admin_user_password    = var.MONGO_PWD
+  auth_type              = "PLAIN_TEXT"
+  shard_capacity         = 2
+  shard_count            = 1
+  subnet_ids             = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id, aws_subnet.subnet_c.id]
+  vpc_security_group_ids = [aws_security_group.mongodb_group.id]
 }
 
