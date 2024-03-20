458f01860e16a30d02fb4732acc33a71666a8a6d
yzhbankov <yzhbankov@redpointpositioning.com>
2024-03-20 02:41:40 +0000

    Fix terraform

--- dev-ops/terraform/output.tf
+++ dev-ops/terraform/output.tf
@@ -6,6 +6,6 @@ output "redis_connection_string" {
   value = "${aws_elasticache_cluster.redis_cluster.cache_nodes.0.address}:${aws_elasticache_cluster.redis_cluster.port}"
 }
 
-#output "mongodb_connection_string" {
-#  value = "mongodb://${aws_docdbelastic_cluster.mongodb_cluster.connection}"
-#}
+output "mongodb_connection_string" {
+  value = "mongodb://${aws_docdbelastic_cluster.mongodb_cluster.endpoint}"
+}
